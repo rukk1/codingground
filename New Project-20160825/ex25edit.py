eee9e63fc8b6e65a017ffd3b250e28f31827990d
rukk1 <thomas.kempster@gmail.com>
2016-09-08 16:19:55 +1000

    Created by Coding Ground Client

---
+++ New Project-20160825/ex25edit.py
@@ -0,0 +1,12 @@
+�
+��Wc@sCd�Zd�Zd�Zd�Zd�Zd�Zd�ZdS(cCs|jd�}|S(s)This function will break up words for us.t (tsplit(tstufftwords((sex25.pytbreak_wordsscCs
+t|�S(sSorts the words.(tsorted(R((sex25.pyt
+sort_wordsscCs|jd�}|GHdS(s+Prints the first word after popping it off.iN(tpop(Rtword((sex25.pytprint_first_word
+scCs|jd�}|GHdS(s&Prints last word after popping it off.i����N(R(RR((sex25.pytprint_last_wordscCst|�}t|�S(s6Takes in a full sentence and returns the sorted words.(RR(tsentenceR((sex25.pyt
+sort_sentencescCs$t|�}t|�t|�dS(s0Prints the first and last words of the sentence.N(RR	R
+(RR((sex25.pytprint_first_and_lasts
+cCs$t|�}t|�t|�dS(s3Sorts the words then prints the first and last one.N(RR	R
+(RR((sex25.pytprint_first_and_last_sorteds
+N(RRR	R
+RR
+R(((sex25.pyt<module>s						
